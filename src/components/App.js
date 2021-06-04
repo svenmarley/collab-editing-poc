f0270609a151b6cc200269025fcbd360d2d3a819
Mike Anderson <mikea@candicontrols.com>
2021-06-04 16:46:18 -0700

    feat(GetDocsList): Lots
    * added getting gAPI set up with config
     - uses a config.js in the src dir
    * Added <Route capabilities
     - Prepped for Login, Login, DocsList, and DocEdit
    * Added msgServer support - so server can send us updates
     - prob needs work

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,44 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { handleInitialData } from '../actions/shared';
-import '../assets/App.css';
 import LoadingBarContainer from 'react-redux-loading';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+
 import Login from './Login';
 import Logout from './Logout';
+import '../assets/App.css';
+
+import { API } from '../utils/_DATA_';
+import { handleInitialData, myFirstDiff } from '../actions/shared';
+import DocsList from './DocsList';
+import DocEdit from './DocEdit';
+
+const config = require( '../config' );
+
+export let gAPI = new API();
+gAPI.setConfig( config.apiServerPath, null );
 
 class App extends Component {
-    sFunc = 'App';
+    #sFunc = 'App';
+    state = {
+        value : '',
+    };
 
     componentDidMount() {
-        const sFunc = this.sFunc + '.componentDidMount()-->';
+        const sFunc = this.#sFunc + '.componentDidMount()-->';
         const debug = false;
 
         debug && console.log( sFunc + 'props', this.props );
 
         this.props.dispatch( handleInitialData() );
+
     }
 
     render() {
-        const sFunc = this.sFunc + '.render()-->';
-        const debug = true;
+        const sFunc = this.#sFunc + '.render()-->';
+        const debug = false;
 
         debug && console.log( sFunc + 'props', this.props );
-        const { authedUser, users } = this.props;
-
-
-        const user = users[authedUser];
-        debug && console.log( sFunc + 'user', user );
-        const userName = ( user ? user.name : '' );
-
 
         return (
             <Router>
@@ -40,9 +48,11 @@ class App extends Component {
                         {this.props.loading === true
                             ? <span>loading</span>
                             : <div>
-                                Welcome: {userName}
                                 <Route path={'/login'} exact component={Login}/>
                                 <Route path={'/logout'} exact component={Logout}/>
+                                <Route path={'/'} exact component={DocsList}/>
+                                <Route path={'/docs'} exact component={DocsList}/>
+                                <Route path={'/docs/:id'} exact component={DocEdit}/>
                             </div>
                         }
                     </div>
@@ -61,7 +71,8 @@ function mapStateToProps( { users, authedUser } ) {
     let bRet = true;
     if ( typeof ( users ) !== 'undefined' ) {
         if ( users.length !== 0 )
-            bRet = false;
+            if ( authedUser )
+                bRet = false;
     }
 
     return {
@@ -70,5 +81,34 @@ function mapStateToProps( { users, authedUser } ) {
         authedUser,
     };
 }
+
+// ******************************************************************************************************
+// ******************************************************************************************************
+// ******************************************************************************************************
+
+//   MESSAGE SERVER --- STUFF
+
+const msgServer = new W3CWebSocket( config.msgServerPath );
+
+msgServer.onopen = () => {
+    console.log( 'WebSocket Client Connected' );
+};
+msgServer.onmessage = ( message ) => {
+    const sFunc = 'msgServer.onmessage()-->';
+    const debug = false;
+
+    debug && console.log( sFunc + 'message', message );
+
+    const m = JSON.parse( message.data );
+
+    const n = myFirstDiff( this.state.value, m.value );
+    debug && console.log( sFunc + 'state.value [%s] m[%s] n', this.state.value, m.value, n );
+
+    this.setState( ( state ) => ( {
+        ...state,
+        value : m.value,
+    } ) );
+
+};
 
 export default connect( mapStateToProps )( App );
