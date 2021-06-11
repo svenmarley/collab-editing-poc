fc1248b68ca0965ed0359a2b8cb4dfce5c34d119
Mike Anderson <mikea@candicontrols.com>
2021-06-11 14:29:38 -0700

    feat:
    Thot I needed to add global users, but removed.  Don't think it's needed

--- src/config.js
+++ src/config.js
@@ -3,3 +3,8 @@ exports.msgServerPath = process.env.ORIGIN || `ws:127.0.0.1:${exports.msgServerP
 
 exports.apiServerPort = process.env.PORT || 3002;
 exports.apiServerPath = process.env.ORIGIN || `http://127.0.0.1:${exports.apiServerPort}`;
+
+// exports.users = {
+//     'alice': 1,
+//     'bob': 2,
+// }
