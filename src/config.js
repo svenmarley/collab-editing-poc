84ec196166b422929cc2bedbdbf8c449e04dcaca
Mike Anderson <mikea@candicontrols.com>
2021-06-14 14:16:56 -0700

    feat(config):
    * adjusted for new dev and prod environments

--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
+const fs = require( 'fs' );
+
+let mainHost;
+
+console.log( 'process.env', process.env );
+
+if ( typeof ( process.env.JAWSDB_MARIA_URL ) === 'undefined' ) {
+    mainHost = '127.0.0.1';
+}
+else {
+    mainHost = 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+}
+
 exports.msgServerPort = process.env.PORT || 3001;
-exports.msgServerPath = process.env.ORIGIN || `ws:127.0.0.1:${exports.msgServerPort}`;
+exports.msgServerPath = process.env.ORIGIN || `ws://${mainHost}:${exports.msgServerPort}`;
 
 exports.apiServerPort = process.env.PORT || 3002;
-exports.apiServerPath = process.env.ORIGIN || `http://127.0.0.1:${exports.apiServerPort}`;
-
-// exports.users = {
-//     'alice': 1,
-//     'bob': 2,
-// }
+exports.apiServerPath = process.env.ORIGIN || `http://${mainHost}:${exports.apiServerPort}`;
