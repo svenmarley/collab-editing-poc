fe891c2d60dc1d9852fc0cf3e84053de458419b7
Mike Anderson <mikea@candicontrols.com>
2021-06-16 18:00:08 -0700

    feat(ConservationEdit component):
    * Figured out how to get the selection length
      - note, selectionEnd
    came back same as selectionStart EVERY TIME
      - had to fake the length
    with the diff in length of the known content and the inbound content.

--- src/components/ConversationEdit.js
+++ src/components/ConversationEdit.js
@@ -13,7 +13,7 @@ class ConversationEdit extends Component {
 
     componentDidMount() {
         const sFunc = this.#sFunc + '.componentDidMount()-->';
-        const debug = true;
+        const debug = false;
 
         const { params } = this.props.match;
         const { dispatch } = this.props;
@@ -22,30 +22,27 @@ class ConversationEdit extends Component {
         gAPI.getConversation( params.id )
             .then( ( conversation ) => {
                 const sFunc = this.#sFunc + '.componentDidMount().gAPI.getConversation.then()-->';
-                const debug = true;
 
                 debug && console.log( sFunc + 'conversation', conversation );
 
                 dispatch( storeConversation( params.id, conversation.content, conversation.lastMutation
                     , conversation.origin ) );
             } );
-
     }
 
     handleChange = ( e ) => {
         const sFunc = this.#sFunc + '.handleChange()-->';
         const debug = false;
 
-        const cursorPos = e.target.selectionStart;
-        debug && console.log( sFunc + 'cursorPosition', cursorPos );
-
-        this.setState( { cursorPosition : cursorPos }, () => {
-            const sFunc = '.setState().then()-->';
-            debug && console.log( sFunc + 'cursor pos set', this.state.cursorPosition );
-        } );
-
         let newValue = e.target.value;
         debug && console.log( sFunc + 'newValue', newValue );
+
+        const { selectionStart, selectionEnd } = e.target;
+        const s = typeof( e.target.onClick );
+        debug && console.log( sFunc + 'selectionStart', selectionStart, 'selectionEnd', selectionEnd, 's', s, 'newValue', newValue );
+        // selectionEnd is ALWAYS the same as selectionStart  todo:  figure out why?
+
+        this.setState( { cursorPosition : selectionStart } );
 
         const { conversation } = this.props;
         debug && console.log( sFunc + 'conversation', conversation );
@@ -54,38 +51,39 @@ class ConversationEdit extends Component {
             // means a char was deleted
             const index = myFirstDiff( conversation.content, newValue );
 
-            const delChar = conversation.content.substring( index, index + 1 );
-            console.log( sFunc + 'DEL( %d, 1 )', index, 'char to be deleted', delChar );
+            const newLen = conversation.content.length - newValue.length;
+            debug && console.log( sFunc + 'index', index, 'selectionStart', selectionStart, 'selectionEnd', selectionEnd, 'newLen', newLen );
 
-            gAPI.sendMutation( 'DEL', conversation.id, conversation.origin, index, 1, '' )
+            const delChars = conversation.content.substring( index, index + 1 );
+            debug && console.log( sFunc + 'DEL( %d, 1 )', index, 'char to be deleted', delChars );
+
+            gAPI.sendMutation( 'DEL', conversation.id, conversation.origin, index, newLen, '' )
                 .then( res => res.json() )
                 .then( ( result ) => {
                     const sFunc = this.#sFunc + '.handleChange().sendInsert().then.then()-->';
 
-                    console.log( sFunc + 'result', result );
+                    debug && console.log( sFunc + 'result', result );
                 } );
         }
         else {
-            const newChar = newValue.substring( cursorPos - 1, cursorPos );
+            const newChar = newValue.substring( selectionStart - 1, selectionStart );
             debug && console.log( sFunc + 'newChar', newChar );
-            console.log( sFunc + 'INS %d:\'%s\'', cursorPos, newChar );
+            debug && console.log( sFunc + 'INS %d:\'%s\'', selectionStart, newChar );
 
-            //gAPI.sendInsert( conversation.id,  conversation.origin, cursorPos - 1, 1, newChar, 1 )
-            gAPI.sendMutation( 'INS', conversation.id, conversation.origin, cursorPos - 1, 1, newChar )
+            //gAPI.sendInsert( conversation.id,  conversation.origin, selectionStart - 1, 1, newChar, 1 )
+            gAPI.sendMutation( 'INS', conversation.id, conversation.origin, selectionStart - 1, 1, newChar )
                 .then( res => res.json() )
                 .then( ( result ) => {
                     const sFunc = this.#sFunc + '.handleChange().sendInsert().then.then()-->';
 
-                    console.log( sFunc + 'result', result );
+                    debug && console.log( sFunc + 'result', result );
                 } );
-
         }
-
     };
 
     componentDidUpdate( prevProps, prevState, snapshot ) {
         const sFunc = 'componentDidUpdate()-->';
-        const debug = true;
+        const debug = false;
 
         const e = document.getElementById( 'theTextAreaIWant' );
 
@@ -95,6 +93,13 @@ class ConversationEdit extends Component {
 
         e.setSelectionRange( this.state.cursorPosition, this.state.cursorPosition );
     }
+
+    // test = (e) => {
+    //     const sFunc = this.#sFunc + '.test()-->';
+    //     const debug = false;
+    //
+    // }
+
 
     render() {
         const sFunc = this.#sFunc + '.render()-->';
@@ -124,6 +129,7 @@ class ConversationEdit extends Component {
                                   id="theTextAreaIWant"
                                   value={conversation.content}
                                   onChange={this.handleChange}
+                                  // onFocus={this.test}
                         >
                     </textarea>
                     </div>
